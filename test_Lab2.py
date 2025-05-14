0350282bc7947a4096212e502da5fd96bb54d2a3
zhanrui <zzhanrui28.24@ichat.sp.edu.sg>
2025-05-14 20:06:53 +0800

    Complete Ex3

---
+++ test_Lab2.py
@@ -0,0 +1,14 @@
+import Lab2 as LAB2
+
+
+def test_find_min_max():
+    datalist = [1, 5, 3, 6, 8, 2, 4]
+    expected_result = [1, 8]
+    test_result = LAB2.find_min_max(datalist)
+
+
+def calc_find_averge():
+
+
+
+def calc_median_temperature():
