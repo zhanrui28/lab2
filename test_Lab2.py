906beebdf720fe4892dda4fc6ce6efce348d9a39
zhanrui <zzhanrui28.24@ichat.sp.edu.sg>
2025-05-26 21:38:55 +0800

    Lab2

--- test_Lab2.py
+++ test_Lab2.py
@@ -5,10 +5,19 @@ def test_find_min_max():
     datalist = [1, 5, 3, 6, 8, 2, 4]
     expected_result = [1, 8]
     test_result = LAB2.find_min_max(datalist)
+    assert (test_result == expected_result)
 
 
-def calc_find_averge():
+def test_calc_averge():
+    datalist = [1, 5, 3, 6, 8, 2, 4]
+    expected_result = 4.14
+    test_result = LAB2.calc_averge(datalist)
+    test_result = round(test_result, 2)
+    assert (test_result == expected_result)
 
 
-
-def calc_median_temperature():
+def test_calc_median_temperature():
+    datalist = [1, 5, 3, 6, 8, 2, 4]
+    expected_result = 4.0
+    test_result = LAB2.calc_median_temperature(datalist)
+    assert (test_result == expected_result)
